7d45b70a47d44220372034b75fdf2befbcfc452e
James Earl Douglas <james@earldouglas.com>
2013-01-24 19:40:25 -0800

    get hello world working

--- project/build.scala
+++ project/build.scala
@@ -21,10 +21,15 @@ object EtchasketchBuild extends Build {
       version := Version,
       scalaVersion := ScalaVersion,
       resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
+      resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
+      resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
       libraryDependencies ++= Seq(
+        "org.eclipse.jetty" % "jetty-websocket" % "8.1.8.v20121106" % "container",
+        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar")),
         "org.scalatra" % "scalatra" % ScalatraVersion,
         "org.scalatra" % "scalatra-scalate" % ScalatraVersion,
         "org.scalatra" % "scalatra-specs2" % ScalatraVersion % "test",
+        "org.scalatra" % "scalatra-atmosphere" % "2.2.0-SNAPSHOT",
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
         "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
--- src/main/scala/SketchApp.scala
+++ src/main/scala/SketchApp.scala
@@ -1,10 +1,27 @@
 package sketch
 
 import org.scalatra._
+import org.scalatra._
+import org.scalatra.atmosphere._                            
+import org.scalatra.json.{JValueResult, JacksonJsonSupport} 
+import org.json4s._                                         
+import JsonDSL._
+import java.util.Date                                       
+import java.text.SimpleDateFormat                           
+import org.fusesource.scalate.Template                      
 
-class SketchApp extends ScalatraServlet {
+class SketchApp extends ScalatraServlet                
+  with JValueResult                     
+  with JacksonJsonSupport with SessionSupport 
+  with AtmosphereSupport  {
+
+  implicit protected val jsonFormats: Formats = DefaultFormats
 
   get("/") {
+    "hello world"
+  }
+
+  get("/hello") {
     "hello world"
   }
 }
